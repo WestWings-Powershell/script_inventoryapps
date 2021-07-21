a1645e175f2583ca12f5e3bbabe7bdf5687f447c
chentsov_vs <chentsov.vyacheslav@gmail.com>
2021-07-21 18:08:00 +0300

    добавил 3 трай \ кетч

--- Script_Get-Apps.ps1
+++ Script_Get-Apps.ps1
@@ -1,6 +1,11 @@
 # Заполняем переменные для работы 
 $Path = "\\SMR01\Share_Z\IT"
 $FileName = "APPsInventory.json"
+$FileError = "Error.txt"
+$MessageError1 = "Блок запросом данных не сработал"
+$MessageError2 = "Блок с записью данных (иф) не сработал"
+$MessageError3 = "Блок с записью данных (елсе) не сработал"
+
 
 # Фильтр условно платных приложений
 $AppFilters = @("Microsoft Office 2010 Standard", `
@@ -24,10 +29,16 @@ $AppFilters = @("Microsoft Office 2010 Standard", `
     "КриптоПро CSP")
 
 # Проводим проверку установленного ПО     
-$DataApps = Get-WMIObject -ClassName Win32_Product
-$PayApps = ForEach ($Filter in $AppFilters){
-    $DataApps.where{$_.name -like "$Filter*"}   
+try {
+    $DataApps = Get-WMIObject -ClassName Win32_Product
+    $PayApps = ForEach ($Filter in $AppFilters){
+        $DataApps.where{$_.name -like "$Filter*"}   
+    }   
 }
+catch {
+    $MessageError1 + '`r`n' + $Error | Out-File $FileError
+}
+
 
 # Формируем хэштаблицу с полученными данными 
 $NewData = [PSCustomObject]@{
@@ -41,25 +52,37 @@ $NewData = [PSCustomObject]@{
 }
 
 # Делаем проверку наличия файа и записываем данные в файл в JSON формате
-$JSONFind = Get-ChildItem -Path $Path\$FileName
-if ($null -eq $JSONFind){
-    $NewData | ConvertTo-Json | Out-File -FilePath $Path\$FileName 
+try {
+    $JSONFind = Get-ChildItem -Path $Path\$FileName
+    if ($null -eq $JSONFind){
+        $NewData | ConvertTo-Json | Out-File -FilePath $Path\$FileName 
+    }
 }
+catch {
+    $MessageError2 + '`r`n' + $Error | Out-File $FileError
+}
+
 
 <# 
     если файл есть - тогда забираем данные из него и 
     преобразуем его обратно в хеш таблицу и 
     перезаписываем обратно уникальные занчения 
 #>
-else {
-    $OldData = Get-Content $Path\$FileName
-    $OldObjectData = $OldData | Select-Object
-    $uniqueComps = ($OldObjectData + $NewData).PCName | Get-Unique 
-    $uniqueData = foreach ($uniqueComp in $uniqueComps) {
-        $Data = $NewData | Where-Object {$_.PCName -like "$uniqueComp"} 
-        $Data
+
+try {
+    else {
+        $OldData = Get-Content $Path\$FileName
+        $OldObjectData = $OldData | Select-Object
+        $uniqueComps = ($OldObjectData + $NewData).PCName | Get-Unique 
+        $uniqueData = foreach ($uniqueComp in $uniqueComps) {
+            $Data = $NewData | Where-Object {$_.PCName -like "$uniqueComp"} 
+            $Data
+        }
+        
+        # Конвертируем обратно и записываем в файл 
+        ConvertTo-Json -InputObject $uniqueData | Out-File $Path\$FileName  -Encoding utf8
     }
-    
-    # Конвертируем обратно и записываем в файл 
-    ConvertTo-Json -InputObject $uniqueData | Out-File $Path\$FileName  -Encoding utf8
+}
+catch {
+    $MessageError3 + '`r`n' + $Error | Out-File $FileError
 }
